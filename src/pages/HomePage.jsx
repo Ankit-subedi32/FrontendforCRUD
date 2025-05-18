0812a9ffa50516e481ff8733f54c15c66a5284a2
Ankit-subedi32 <ankit.subedi12322@gmail.com>
2025-05-18 22:20:03 +0545

    first commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,31 @@
-import Button from "../components/Button"
+import axios from "axios"
 import Card from "../components/Card"
 import Navbar from "../components/Navbar"
+import { use, useEffect } from "react"
 
 
 
 function HomePage() {
+  const  fetchBooks = async () => {
+       const response = await axios.get("http://localhost:5000/api/books")
+       
+    }
+  useEffect(() => {
+    fetchBooks()    
+    }, [])
+    // const [books, setBooks] = useState([])
+    
     return (
         <>
 
         <Navbar />
         <div className="flex items-center ml-40 pb-30 h-screen flex-wrap"> 
          
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
         <Card />
         <Card />
         <Card />
