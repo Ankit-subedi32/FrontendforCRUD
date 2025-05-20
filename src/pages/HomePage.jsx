e00ac4907a19ce1e9b7722475459b19437a112f7
Ankit-subedi32 <ankit.subedi12322@gmail.com>
2025-05-20 20:29:19 +0545

    second

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,16 @@ import axios from "axios"
 import Card from "../components/Card"
 import Navbar from "../components/Navbar"
 import { use, useEffect } from "react"
+import { useState } from "react"
+
 
 
 
 function HomePage() {
+  const [books, setBooks] = useState([])
   const  fetchBooks = async () => {
        const response = await axios.get("http://localhost:5000/api/books")
-       
+       setBooks(response.data.data)
     }
   useEffect(() => {
     fetchBooks()    
@@ -20,19 +23,15 @@ function HomePage() {
 
         <Navbar />
         <div className="flex items-center ml-40 pb-30 h-screen flex-wrap"> 
-         
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+         {
+          books.map(function(book){
+            return (
+
+              <Card  book = {book} />
+            )
+          })
+         }
+       
 </div>
     
         
